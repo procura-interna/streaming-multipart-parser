99ab454ba92a2f64d340dee2a628ddc68de9a13f
Procura Interna <127160648+procura-interna@users.noreply.github.com>
2025-05-15 18:41:50 +0100

    Boyer–Moore–Horspool search algorithm

---
+++ src/main/java/dev/baecher/BmhSearch.java
@@ -0,0 +1,63 @@
+package dev.baecher;
+
+import java.util.Arrays;
+
+public final class BmhSearch {
+
+  private BmhSearch() {
+  }
+
+
+  /**
+   * Searches for the first occurrence of a byte pattern in a byte array. Uses the
+   * Boyer–Moore–Horspool algorithm for linear time complexity
+   */
+  public static int search(final byte[] reference, final int referenceStart, final int referenceEnd,
+      final byte[] toSearch) {
+    if (toSearch.length == 0) {
+      return 0;
+    }
+
+    final int[] bmhPattern = preprocessBmhPattern(toSearch);
+
+    int skip = referenceStart;
+    while (reference.length - skip >= toSearch.length) {
+      if (bmhSame(reference, skip, referenceEnd, toSearch)) {
+        return skip;
+      }
+
+      skip = skip + bmhPattern[reference[skip + toSearch.length - 1]];
+    }
+
+    return -1;
+  }
+
+  private static boolean bmhSame(final byte[] reference, final int referenceStart,
+      final int referenceEnd, final byte[] toSearch) {
+
+    for (int r = referenceStart + toSearch.length - 1, s = toSearch.length - 1; s < toSearch.length; r--, s--) {
+      if (reference[r] != toSearch[s]) {
+        break;
+      }
+
+      if (s == 1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private static int[] preprocessBmhPattern(final byte[] toSearch) {
+    final int bmhPatternLength = 256;
+    final int[] bmhPattern = new int[bmhPatternLength];
+
+    Arrays.fill(bmhPattern, toSearch.length);
+
+    for (int i = 0; i < toSearch.length - 1; i++) {
+      bmhPattern[toSearch[i]] = toSearch.length - 1 - i;
+    }
+
+    return bmhPattern;
+  }
+}
